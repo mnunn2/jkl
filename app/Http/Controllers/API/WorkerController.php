c76459ccc34d69d3bc549dd2692adc7410796bff
mnunn2 <bonnie650@gmail.com>
2018-10-28 18:33:25 +0000

    started adding routes for api

---
+++ app/Http/Controllers/API/WorkerController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Worker;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WorkerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $workers = Worker::all();
+        return response()->json($workers);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Worker  $worker
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Worker $worker)
+    {
+        // $worker = Worker::where('last_name', 'Casper')->first();
+        return response()->json($worker);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Worker  $worker
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Worker $worker)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Worker  $worker
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Worker $worker)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('workers', 'API\WorkerController@index');
+Route::get('worker/{worker}', 'API\WorkerController@show');
+
+//Route::apiResources([
+//    'workers' => 'API\WorkerController'
+//]);
+
+
