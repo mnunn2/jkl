a6ffbd168a9ce0a5d292d668c76dfa247bd2e5f3
mnunn2 <bonnie650@gmail.com>
2018-11-07 19:46:51 +0000

    added passport and make auth

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+                <div class="card-body">
+                    <Passport-clients></Passport-clients>
+                    <Passport-authorized-clients></Passport-authorized-clients>
+                    <Passport-personal-access-tokens></Passport-personal-access-tokens>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
