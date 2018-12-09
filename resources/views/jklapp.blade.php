51d1eb59737891cf91ca2f43a9dfa431af4db58d
mnunn2 <bonnie650@gmail.com>
2018-12-09 17:06:32 +0000

    removed source maps from mix

--- resources/views/jklapp.blade.php
+++ resources/views/jklapp.blade.php
@@ -8,10 +8,8 @@
 
     <title>JKL</title>
 
-    {{--<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">--}}
     <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
-    {{--<link rel="stylesheet" type="text/css" href="css/app.css">--}}
 </head>
 <body>
 <div id="fred">
