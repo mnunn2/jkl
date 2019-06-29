18e22b11e181a11202bf1496bb0e38e390ede819
mnunn2 <bonnie650@gmail.com>
2019-06-29 16:34:31 +0100

    updated login screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,31 +2,34 @@
 
 @section('content')
 
-    <form class="login" method="POST" action="{{ route('login') }}">
-        @csrf
-        <h1 class="login-title">Login</h1>
+    <div id=login-main>
 
-        <input id="email" type="email" name="email" class="login-input" placeholder="Email Address" required autofocus>
+        <form class="login" method="POST" action="{{ route('login') }}">
+            @csrf
+            <h1 class="login-title">Login</h1>
 
-        @if ($errors->has('email'))
-            <span > <strong>{{ $errors->first('email') }}</strong> </span>
-        @endif
+            <input id="email" type="email" name="email" class="login-input" placeholder="Email Address" required autofocus>
 
-        <input id="password" type="password" class="login-input" name="password" placeholder="Password" required>
+            @if ($errors->has('email'))
+                <span > <strong>{{ $errors->first('email') }}</strong> </span>
+            @endif
 
-        @if ($errors->has('password'))
-            <span > <strong>{{ $errors->first('password') }}</strong> </span>
-        @endif
+            <input id="password" type="password" class="login-input" name="password" placeholder="Password" required>
 
-        <button type="submit" class="login-button" > {{ __('Login') }} </button>
+            @if ($errors->has('password'))
+                <span > <strong>{{ $errors->first('password') }}</strong> </span>
+            @endif
 
-        <p class="login-lost">
-            <a  href="{{ route('password.request') }}"> {{ __('Forgot Your Password?') }} </a>
-        </p>
-        <p class="login-lost">
-            <label for="remember"> {{ __('Remember Me') }} </label>
-            <input  type="checkbox" name="remember" id="remember">
-        </p>
+            <button type="submit" class="login-button" > {{ __('Login') }} </button>
 
-    </form>
+            <p class="login-lost">
+                <a  href="{{ route('password.request') }}"> {{ __('Forgot Your Password?') }} </a>
+            </p>
+            <p class="login-lost">
+                <label for="remember"> {{ __('Remember Me') }} </label>
+                <input  type="checkbox" name="remember" id="remember">
+            </p>
+        </form>
+
+    </div>
 @endsection
