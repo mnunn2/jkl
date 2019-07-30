7079dd03d3b690171dc9010a23be0aeb3ddd4fe0
mnunn2 <bonnie650@gmail.com>
2019-07-30 14:43:55 +0100

    fixed broken cdn css link after v2 released

--- resources/views/jklapp.blade.php
+++ resources/views/jklapp.blade.php
@@ -9,7 +9,8 @@
     <title>JKL</title>
 
     <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet"> -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/vuetify/1.5.16/vuetify.min.css" rel="stylesheet">
 </head>
 <body>
 <div id="fred">
